df2694b408044ccec7909ca417c4bb95a9ac1dc0
atilao4501 <atilafa2012@hotmail.com>
2024-04-15 16:47:21 -0300

    transferindo entre computadores 15_04

---
+++ SuperHeroAPI-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_DotNet8.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace SuperHeroAPI_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<SuperHero>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Invincible",
+                    FirstName = "Mark",
+                    LastName = "Greyson",
+                    Place = "Viltrum Earth"
+            
+                }
+            };
+            
+            return Ok(heroes);
+        }
+    }  
+}
---
+++ SuperHeroAPI-DotNet8/Entities/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroAPI_DotNet8.Entities;
+
+public class SuperHero
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; } 
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string Place { get; set; } = String.Empty;
+}
